3e6cff36c5ea9f6a387892a577f817c7a4e357d4
WELLINGTON MARTEVI DE LIMA <t-wellington.m.lima@bradesco.com.br>
2022-07-27 17:26:10 -0300

    Anime-15

--- src/main/java/martevi/devspring/projectspringboot/domain/Anime.java
+++ src/main/java/martevi/devspring/projectspringboot/domain/Anime.java
@@ -2,10 +2,21 @@ package martevi.devspring.projectspringboot.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
